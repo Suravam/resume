055733acc2a2fee013598b1cc8eb9defa94258cf
Suravam <kumar.dsaravana@gmail.com>
2020-08-02 20:16:11 +0530

    Web Resume in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import { AppComponent } from './app.business-card';
+import { AchievementsComponent, HybridDetails } from './app.achievements-showcase';
+import { AppMaterialModule } from "./app-material.module";
+import { ChartsModule } from 'ng2-charts';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AchievementsComponent,
+    HybridDetails
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    AppMaterialModule,
+    ChartsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, AchievementsComponent],
+  entryComponents: [
+    HybridDetails
+  ]
 })
 export class AppModule { }
